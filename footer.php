7b624faaa7e7f195333bf1804848912765983d69
githubfollowme <flymayday112@gmail.com>
2021-12-15 10:40:35 +0800

    update new repository

---
+++ footer.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "<p>Copyright © 2001-" . date("Y") . "泰山公司專用</p>";
+?>
---
+++ result_page.php
@@ -0,0 +1,14 @@
+<div>
+        <?php include 'header.php';?>
+    </div>
+<?php
+
+$firstName=$_GET['firstName'];
+$secondName=$_GET['secondName'];
+$result;
+echo "計算結果為 ".$result;
+?>
+
+<div>
+    <?php include 'footer.php';?>
+</div>
